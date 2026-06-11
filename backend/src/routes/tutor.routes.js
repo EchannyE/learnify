2b1c27fb6aeabb9c6fcf1087809f424127b39c56
EchannyE <ondaechanny@outlook.com>
2026-06-11 21:58:03 +0100

    Update tutor.routes.js

--- backend/src/routes/tutor.routes.js
+++ backend/src/routes/tutor.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { askTutor } from "../controllers/tutor.controller.js";
+import { ask } from "../controllers/tutor.controller.js";
 import { protect } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
